c8f10ca6a41e7f0d571d673991ea59a1e2bae623
Neil Larion <neil.larion@gmail.com>
2016-06-20 15:09:21 -0700

    add coolCtrl and imAsibling examples to the project

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,17 @@ angular.module("todoListApp",[])
 	$scope.helloWorld = function(){
 		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
 	};
+})
+.controller('coolCtrl', function($scope){
+	$scope.whoAmI = function(){
+		console.log("Hello there, this is the coolCtrl function!");
+	};
+	//you can override methods!!!!
+	$scope.helloWorld = function(){
+		console.log("This is not the main ctrl!");
+	}
+})
+.controller('imASibling', function($scope){
+	$scope.foobar = 1234;
+	//do some cool stuff here!!!
 });
