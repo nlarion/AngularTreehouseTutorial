8a765181a25480d394c119b0f3b233e48c7e3c47
Neil Larion <neil.larion@gmail.com>
2016-06-21 16:52:44 -0700

    Update todo app to include ng-change, ng-show and ng-blur directives and update styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,14 @@
 angular.module("todoListApp",[])
 .controller('mainCtrl', function($scope){
-	$scope.helloWorld = function(){
-		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
+	$scope.learningNgChange = function(){
+		console.log("an input changed!");
 	};
-})
-.controller('coolCtrl', function($scope){
-	$scope.whoAmI = function(){
-		console.log("Hello there, this is the coolCtrl function!");
-	};
-	//you can override methods!!!!
-	$scope.helloWorld = function(){
-		console.log("This is not the main ctrl!");
-	}
-})
-.controller('imASibling', function($scope){
-	$scope.foobar = 1234;
-	//do some cool stuff here!!!
+	$scope.todos = [
+		{"name":"cleanm the aslkjasdkljasdf"},
+		{"name":"water the asdihasdjhioasdfoij"},
+		{"name":"dog the walk"},
+		{"name":"poop"},
+		{"name":"pick up sticks"}
+
+	]
 });
